8d00bf05ca6d1cc0743651d4b32663f646b72860
Bradley Wogsland <bradley@wogsland.org>
2016-04-21 13:16:57 -0500

    brought in Database classes

---
+++ Database/LandingPage.php
@@ -0,0 +1,48 @@
+<?php
+namespace Sizzle\Bacon\Database;
+
+/**
+ * This class is for database interaction with landing pages.
+ */
+class LandingPage extends \Sizzle\Bacon\DatabaseEntity
+{
+    protected $script;
+
+    /**
+     * This function constructs the class from a random landing page
+     * the specified id.
+     *
+     * @param int $id - optional id of the landing_page
+     */
+    public function __construct(int $id = null)
+    {
+        if ($id !== null) {
+            $id = (int) $id;
+            $condition = "AND id = '$id'";
+        } else {
+            $condition = "ORDER BY RAND() LIMIT 1";
+        }
+        $page = execute_query(
+            "SELECT * FROM landing_page
+            WHERE deleted IS NULL
+            $condition"
+        )->fetch_object();
+        if (is_object($page)) {
+            foreach (get_object_vars($page) as $key => $value) {
+                $this->$key = $value;
+            }
+        }
+    }
+
+    /**
+     * Records a hit on the landing page represented by this class
+     *
+     * @param string $visitor_cookie - cookie identifying the visitor
+     *
+     * @return boolean - success of recording
+     */
+    public function recordHit(string $visitor_cookie)
+    {
+        (new LandingPageView())->create($this->id, $visitor_cookie);
+    }
+}
---
+++ Database/Support.php
@@ -0,0 +1,26 @@
+<?php
+namespace Sizzle\Bacon\Database;
+
+/**
+ * This class is for database interaction with sent support email.
+ */
+class Support extends \Sizzle\Bacon\DatabaseEntity
+{
+    protected $email_address;
+    protected $message;
+
+    /**
+     * Saves a receipt of the sent support mail..
+     *
+     * @param $email_address
+     * @param $message
+     */
+    public static function create(string $email_address, string $message)
+    {
+        $Support = new Support();
+        $Support->email_address = $email_address;
+        $Support->message = $message;
+        $Support->save();
+        return $Support;
+    }
+}
